d8db18133a8dfbcc4effc4cc9f3fc3f29135a62d
Bao Vu <baodvu@gmail.com>
2018-10-28 11:48:07 -0400

    Change polling interval and logging to console

--- codesignal/challenge_notification.js
+++ codesignal/challenge_notification.js
@@ -5,7 +5,7 @@ const {isProdEnv} = require('./env.js');
 nodemon({
   script: isProdEnv() ? '/root/codebase/codesignal/challenge_notification_script.js' : './challenge_notification_script.js',
   ext: 'js json',
-  stdout: !isProdEnv() // important: this tells nodemon not to output to console
+  // stdout: !isProdEnv() // important: this tells nodemon not to output to console
 });
 // .on('readable', function() { // the `readable` event indicates that data is ready to pick up
   // if (isProdEnv()) {
